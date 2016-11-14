0301e3bc1ea1a55592a4978fa35d556130586a4a
HusterRichard <zxc1206@live.cn>
2016-11-15 00:23:25 +0800

    模型和接口

---
+++ doctor/src/main/java/com/huawei/db/HeathDataRepository.java
@@ -0,0 +1,10 @@
+//package com.huawei.db;
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//import com.huawei.model.HeathData;
+//import com.huawei.model.User;
+//
+//public interface HeathDataRepository extends MongoRepository<HeathData, String>{
+//	public User findByNameAndTime(String name, String collectionTime);
+//}
---
+++ doctor/src/main/java/com/huawei/model/BodyProblem.java
@@ -0,0 +1,56 @@
+package com.huawei.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * 常规问题库<br>
+ * @author zhangxincheng
+ *
+ */
+public class BodyProblem {
+	
+	@Id
+	private String id;
+	
+	// #region Fields
+	
+	// 检索关键字列表
+	private List<String> lstKeyWord;
+	
+	// 问题答案
+	private String answer;
+	
+	// #endregion
+	
+	// #region getter/setter
+
+	public List<String> getLstKeyWord() {
+		return lstKeyWord;
+	}
+
+	public void setLstKeyWord(List<String> lstKeyWord) {
+		this.lstKeyWord = lstKeyWord;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	// #endregion
+	
+	// #region Constructor
+	
+	public BodyProblem(){
+		
+	}
+	
+	// #endregion
+	
+}
+
